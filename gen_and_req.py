c900d972612d58788799a0331fcc0ed3e99f17ff
Carl Jason <cj123inc@gmail.com>
2025-06-23 23:02:47 +0530

    Scorebar, client whatsapp message intimation, editable pdf link

--- gen_and_req.py
+++ gen_and_req.py
@@ -56,14 +56,58 @@ auditDetails = {
     "ovr": ""
 }
 
+scoreBarElements = {
+    "pb_s": {
+        "poor": "g36773a6a01a_0_2",
+        "average": "g36773a6a01a_0_5",
+        "amazing": "p1_i117"
+    },
+    "cs_s": {
+        "poor": "g36773a6a01a_0_14",
+        "average": "g36773a6a01a_0_11",
+        "amazing": "p1_i121"
+    },
+    "es_s": {
+        "poor": "g36773a6a01a_0_20",
+        "average": "p1_i129",
+        "amazing": "g36773a6a01a_0_17"
+    },
+    "po_s": {
+        "poor": "p1_i133",
+        "average": "g36773a6a01a_0_26",
+        "amazing": "g36773a6a01a_0_23"
+    },
+    "ape_s": {
+        "poor": "g36773a6a01a_0_29",
+        "average": "g36773a6a01a_0_32",
+        "amazing": "p1_i125"
+    },
+}
 
-def convert_and_round_down(x):
-    if not 0 <= x <= 10:
-        raise ValueError("Input must be between 0 and 10.")
-    return int(x) * 10
+
+def mask_email(email):
+    user, domain = email.split("@")
+    return f"{user[0]}***@{domain}"
 
 
-def start_process(sheetName = 'Template'):
+def mask_phone(phone):
+    return f"{'*' * (len(phone) - 4)}{phone[-4:]}"
+
+
+def get_score_description(score):
+    if score < 50:
+        return "Poor"
+    elif score < 70:
+        return "Average"
+    else:
+        return "Amazing"
+
+
+def scale_and_round(score):
+    return round(score * 10 / 5) * 5
+
+
+def start_process(sheetName='29 Ambuj S'):
     try:
         # Get data from spreadsheet
         client = gspread.authorize(creds)
@@ -73,40 +117,34 @@ def start_process(sheetName = 'Template'):
         auditDetails["followers"] = sheet.acell("C39").value
         auditDetails["title"] = sheet.acell("C40").value
         auditDetails["recommendations"] = sheet.acell("C42").value.strip()
+        image_url = sheet.acell("C41").value
 
         sum = 0
-        auditDetails["pb_s"] = convert_and_round_down(
+        auditDetails["pb_s"] = scale_and_round(
             float(sheet.acell("D8").value))
         sum += float(sheet.acell("D8").value)
-        auditDetails["cs_s"] = convert_and_round_down(
+        auditDetails["cs_s"] = scale_and_round(
             float(sheet.acell("D23").value))
         sum += float(sheet.acell("D23").value)
-        auditDetails["es_s"] = convert_and_round_down(
+        auditDetails["es_s"] = scale_and_round(
             float(sheet.acell("D35").value))
         sum += float(sheet.acell("D35").value)
-        auditDetails["po_s"] = convert_and_round_down(
+        auditDetails["po_s"] = scale_and_round(
             float(sheet.acell("D30").value))
         sum += float(sheet.acell("D30").value)
-        auditDetails["ape_s"] = convert_and_round_down(
+        auditDetails["ape_s"] = scale_and_round(
             float(sheet.acell("D18").value))
         sum += float(sheet.acell("D18").value)
-        auditDetails["os_s"] = convert_and_round_down(sum / 5)
-
-        if auditDetails["os_s"] <= 40:
-            auditDetails["ovr"] = "Poor"
-        elif auditDetails["os_s"] <= 60:
-            auditDetails["ovr"] = "Good"
-        elif auditDetails["os_s"] <= 80:
-            auditDetails["ovr"] = "Great"
-        else:
-            auditDetails["ovr"] = "Excellent"
+        auditDetails["os_s"] = scale_and_round(sum / 5)
+        auditDetails["ovr"] = get_score_description(auditDetails["os_s"])
 
         recipient_email = sheet.acell("C43").value
+        phone_number = sheet.acell("C42").value
 
-        # Create new report and enter data
         slides_service = build('slides', 'v1', credentials=creds)
         drive_service = build('drive', 'v3', credentials=creds)
 
+        # Create new report
         copy_response = drive_service.files().copy(
             fileId=PRESENTATION_ID,
             body={"name": f"Temp Copy for {auditDetails['name']}"}
@@ -117,6 +155,7 @@ def start_process(sheetName = 'Template'):
             "requests": []
         }
 
+        # Insert scores
         for key, value in auditDetails.items():
             placeholder = key.upper()
             repValue = str(value)
@@ -134,6 +173,7 @@ def start_process(sheetName = 'Template'):
                 }
             })
 
+        # Insert recommendations
         requests_body["requests"].append(
             {
                 "createParagraphBullets": {
@@ -145,6 +185,39 @@ def start_process(sheetName = 'Template'):
                 }
             })
 
+        # Insert picture
+        requests_body["requests"].append(
+            {
+                "replaceAllShapesWithImage": {
+                    "imageUrl": image_url,
+                    "replaceMethod": "CENTER_INSIDE",
+                    "containsText": {
+                        "text": "[[IMAGE]]",
+                        "matchCase": True
+                    }
+                }
+            })
+
+        # Hide irrelevant score bars
+        for key in ["pb_s", "cs_s", "es_s", "po_s", "ape_s"]:
+            desc = get_score_description(auditDetails[key])
+            for scoreKey in scoreBarElements[key].keys():
+                if desc.lower() != scoreKey:
+                    requests_body["requests"].append({
+                        "updatePageElementTransform": {
+                            "objectId": scoreBarElements[key][scoreKey],
+                            "applyMode": "ABSOLUTE",
+                            "transform": {
+                                "scaleX": 1,
+                                "scaleY": 1,
+                                "translateX": 5000,
+                                "translateY": 5000,
+                                "unit": "PT"
+                            }
+                        }
+                    })
+
+        # Update slide
         slides_service.presentations().batchUpdate(
             presentationId=copy_presentation_id,
             body=requests_body
@@ -153,15 +226,21 @@ def start_process(sheetName = 'Template'):
         # Update report permissions and name
         fileUpdates = {
             'type': 'anyone',
-            'role': 'reader',
+            'role': 'writer',
             'name': f"{auditDetails["name"].replace(" ", "_")}_profile_audit"
         }
 
+        # Create slide
         drive_service.permissions().create(
             fileId=copy_presentation_id,
             body=fileUpdates
         ).execute()
 
+        editor_link = f"https://docs.google.com/presentation/d/{copy_presentation_id}/edit"
+        # sheet.update_acell("C49", editor_link)
+        return
+
+        # # Show all elements in slide
         # presentation = slides_service.presentations().get(
         #     presentationId=copy_presentation_id).execute()
         # for idx, slide in enumerate(presentation.get('slides')):
@@ -181,45 +260,38 @@ def start_process(sheetName = 'Template'):
         #         print(
         #             f"- ID: {obj_id} | Type: {shape_type} | Text: '{text.strip()}'")
 
-        # request = drive_service.files().export_media(
-        #     fileId=copy_presentation_id, mimeType='application/pdf')
+        # Download PDF
+
+        request = drive_service.files().export_media(
+            fileId=copy_presentation_id, mimeType='application/pdf')
+        pdf_file = request.execute()
+
+        name = auditDetails["name"].replace(" ", "_")
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"{name}_{timestamp}.pdf"
+
+        with open(f"pdfs/{filename}", 'wb') as f:
+            f.write(pdf_file)
+
+        print(f"PDF downloaded as {filename}")
+
+        # # PDF link
+        # pdf_link = f"https://docs.google.com/presentation/d/{
+        #     copy_presentation_id}/export/pdf"
         #
-        # name = auditDetails["name"].replace(" ", "_")
-        # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        #
-        # filename = f"{name}_{timestamp}.pdf"
+        # sheet.update_acell("C45", pdf_link)
+        return
 
-        # fh = io.BytesIO()
-        # downloader = MediaIoBaseDownload(fh, request)
-        #
-        # done = False
-        # while not done:
-        #     status, done = downloader.next_chunk()
-        #
-        # with open("your_filename.pdf", "wb") as f:
-        #     f.write(fh.getbuffer())
-        # pdf_file = request.execute()
-        # with open(f"pdfs/{filename}", 'wb') as f:
-        #     f.write(pdf_file)
-
-        # print(f"PDF downloaded as {filename}")
-
-        # PDF link
-        pdf_link = f"https://docs.google.com/presentation/d/{
-            copy_presentation_id}/export/pdf"
-
-        sheet.update_acell("C45", pdf_link)
-
-        # Send WhatsApp message via Twilio
+        # Send WhatsApp message via Twilio for approval
         twilio_client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
         message = twilio_client.messages.create(
             from_=f"{TWILIO_WHATSAPP_NUMBER}",
             to=f"{YOUR_WHATSAPP}",
             body=f"{auditDetails["name"]}'s report is ready: {
-                pdf_link}\nReply 'yes' to approve and send to {recipient_email}",
+                pdf_link}\nIf you wish to make changes to the document, click this link to edit and save the document: {editor_link}\nReply 'yes' to approve and send to {recipient_email}",
             media_url=[pdf_link]
         )
-        print("Message sent. SID:", message)
+        print("Approval message sent. SID:", message)
 
         start_time = time.time()
         approved = False
@@ -231,14 +303,13 @@ def start_process(sheetName = 'Template'):
         while time.time() - start_time < 300:  # 5 minutes timeout
             messages = twilio_client.messages.list(
                 to=f"{TWILIO_WHATSAPP_NUMBER}", limit=5)
-            print(messages)
             for msg in messages:
-                print('datesenttime ', msg.date_sent)
                 if msg.direction == "inbound" and msg.date_sent > curDatetime and msg.from_ == f"{YOUR_WHATSAPP}":
                     if msg.body.strip().lower() in ["yes", "approve"]:
                         approved = True
                         print("Approval received.")
                         send_email(recipient_email, pdf_link)
+                        send_w_message(phone_number, pdf_link)
                         # Delete pdf
                         # drive_service.files().delete(fileId=copy_presentation_id).execute()
                         break
@@ -254,6 +325,7 @@ def start_process(sheetName = 'Template'):
         print(error)
 
 
+# Send email to client
 def send_email(to_email, pdf_url):
     try:
         msg = MIMEText(f"Hi {auditDetails['name']},\n\nAttached is your LinkedIn profile audit. It outlines what's working, and where a few smart tweaks can amplify how you're seen by the right audience.\n\nYou already bring the credibility - this helps make it visible.\n\nLet me know what you think. Happy to walk you through it anytime.\n\nBest,\nAmbuj\n\n{pdf_url}")
@@ -265,12 +337,28 @@ def send_email(to_email, pdf_url):
             server.login(EMAIL_FROM, EMAIL_PASSWORD)
             server.sendmail(EMAIL_FROM, to_email, msg.as_string())
 
-        print("Email sent to", to_email)
+        print(f"Email sent to client's address at {mask_email(to_email)}")
+
+    except Exception as e:
+        print(e)
+
+
+# Send whatsapp message to client
+def send_w_message(phone_num, pdf_url):
+    try:
+        message = client.messages.create(
+            from_='whatsapp:+15557957011',
+            to=f'whatsapp:+91${phone_num}',
+            body='Template message'
+        )
+
+        print(f"Message sent to client's whatsapp at {mask_phone(phone_num)}.")
 
     except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
-    start_process()
-    # send_email("carljason.3012@gmail.com", "https://example.com")
+    # start_process()
+    send_email("carljason.3012@gmail.com", "https://docs.google.com/presentation/d/1HEpifNk3pTArN2qERbBX75dhOCbgSWLMIjkmGXrHxxU/export/pdf")
+    # send_w_message("9942727949", "https://example.com");
